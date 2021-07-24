1e6f9e3f220c60fe6e94d590ebffa89a8c61abf0
sanjoy <paulsanjoy9875@gmail.com>
2021-07-24 11:56:34 +0530

    splash screen

--- app/src/main/java/com/example/roomdatabasewithmvvm/MainActivity.kt
+++ app/src/main/java/com/example/roomdatabasewithmvvm/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.roomdatabasewithmvvm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val splashTimeOut :Int = 3000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar!!.hide()
+
+        progressBar.visibility = View.VISIBLE
+
+        Handler().postDelayed({
+            val intent = Intent(this@MainActivity, ShowItemActivity::class.java)
+            startActivity(intent)
+            finish()
+            progressBar.visibility = View.GONE
+        }, splashTimeOut.toLong())
+
     }
 }
